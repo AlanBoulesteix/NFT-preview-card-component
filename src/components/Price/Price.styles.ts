b28774fa7fc03acd4e01f51ff1d1d470d136add4
AlanBoulesteix <alan.boulesteix@gmail.com>
2025-01-31 14:04:20 -0500

    Add Price component

---
+++ src/components/Price/Price.styles.ts
@@ -0,0 +1,11 @@
+import { styled } from 'styled-components';
+
+export const PriceWrapper = styled.div`
+  display: flex;
+  align-items: center;
+  gap: 0.5rem;
+`;
+
+export const PriceValue = styled.p`
+  color: var(--accent-color);
+`;
---
+++ src/components/Price/Price.tsx
@@ -0,0 +1,18 @@
+import { IconsName } from '../Icons/enum/IconsName';
+import Icon from '../Icons/Icon';
+import { PriceValue, PriceWrapper } from './Price.styles';
+
+type PriceParm = {
+  price: string;
+};
+
+const Price = ({price}: PriceParm): JSX.Element => {
+  return (
+    <PriceWrapper>
+      <Icon name={IconsName.ethereum} />
+      <PriceValue>{price}</PriceValue>
+    </PriceWrapper>
+  );
+};
+
+export default Price;
