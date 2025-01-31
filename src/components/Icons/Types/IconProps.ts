2e2fb4b494c35a5aaee5e920e9c1e65dfee613d1
AlanBoulesteix <alan.boulesteix@gmail.com>
2025-01-31 14:17:55 -0500

    Rename Enum

--- src/components/Icons/Types/IconProps.ts
+++ src/components/Icons/Types/IconProps.ts
@@ -1,6 +1,6 @@
-import { IconEnum } from '../enum/IconEnum';
+import { IconsName } from '../enum/IconsName';
 import { SubIconProps } from './SubIconProps';
 
 export interface IconProps extends SubIconProps {
-  name: IconEnum;
+  name: IconsName;
 }
