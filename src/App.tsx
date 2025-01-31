e1f92f537f7113ac872705609dc62206c845ccc1
AlanBoulesteix <alan.boulesteix@gmail.com>
2025-01-31 15:53:02 -0500

    Make app more re-usable

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,26 @@
 import { CardsWrapper } from './App.styles';
 import NftCard from './components/NftCard/NftCard';
+import nftData from './data/nftData.json';
 
 function App() {
+
+  const NftList = nftData;
+
   return (
     <>
       <CardsWrapper>
-        <NftCard />
+        {NftList.map((nft) => (
+          <NftCard
+            key={nft.id}
+            imgUrl={nft.imageUrl}
+            title={nft.title}
+            description={nft.description}
+            price={nft.price}
+            date={nft.date}
+            creatorImgUrl={nft.creator.imageUrl}
+            creatorName={nft.creator.name}
+          />
+        ))}
       </CardsWrapper>
     </>
   );
